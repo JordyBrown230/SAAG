d3daf0c305494ff3c06e431842a1d2e89af012fe
Francisco Ortiz <jose.ortiz.ortiz@est.una.ac.cr>
2023-10-17 02:18:26 -0600

    cambios - VERIFICAR .ENV.EXAMPLE

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
-const sequelize = new Sequelize('acib', 'root', '', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+    host: process.env.DB_HOST,
     dialect: 'mysql',
     define: {
         freezeTableName: true// para que no agruegue la S tipo solicitud's', al crear el schema en la db xd.
