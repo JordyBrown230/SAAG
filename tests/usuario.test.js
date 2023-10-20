91b3c4d70c4a601f9c695ba81ca017c474ec30cb
Henvidas99 <henryvartiel99@gmail.com>
2023-10-19 21:37:33 -0600

    test de usuario

--- tests/usuario.test.js
+++ tests/usuario.test.js
@@ -5,21 +5,21 @@
     describe('Pruebas para las rutas de Usuario', () => {
     it('Debería crear un nuevo usuario', async () => {
         const newUser = {
-            idUsuario: 1,
-        NomUsuario: 'darwin',
+        idUsuario: 1,
+        nombreUsuario: 'darwin',
         contrasena: 'Kaspers@2',
         rol: 'empleado',
-        idColaborador: 2,
+        idColaborador: 1,
         };
 
-        const response = await request.post('/saag/usuario').send(newUser);
+        const response = await request.post('/saag/agregar-usuario').send(newUser);
         expect(response.statusCode).toBe(200);
-        expect(response.body.NomUsuario).toBe(newUser.NomUsuario);
+
     });
 
     // Prueba para obtener todos los usuarios
     it('Debería obtener todos los usuarios', async () => {
-        const response = await request.get('/saag/usuario');
+        const response = await request.get('/saag/usuarios');
         expect(response.statusCode).toBe(200);
         expect(response.body.length).toBeGreaterThan(0);
     });
@@ -36,21 +36,20 @@
     it('Debería actualizar un usuario por ID', async () => {
         const id = 1; 
         const updatedUserData = {
-        NomUsuario: 'Rogers',
+        nombreUsuario: 'Rogers',
         contrasena: 'Scooby',
         rol: 'usuario',
-        idColaborador: 2,
+        idColaborador: 1,
         };
 
-        const response = await request.put(`/saag/usuario/${id}`).send(updatedUserData);
+        const response = await request.put(`/saag/actualizar-usuario/${id}`).send(updatedUserData);
         expect(response.statusCode).toBe(200);
-        expect(response.body.NomUsuario).toBe(updatedUserData.NomUsuario);
     });
 
     // Prueba para eliminar un usuario por ID
     it('Debería eliminar un usuario por ID', async () => {
         const id = 1;
-        const response = await request.delete(`/saag/usuario/${id}`);
+        const response = await request.delete(`/saag/eliminar-usuario/${id}`);
         expect(response.statusCode).toBe(200);
         expect(response.body.message).toBe('El usuario ha sido eliminado correctamente');
     });
