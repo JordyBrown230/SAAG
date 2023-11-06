20126c41acdde1236f15ba4b28d89622c9c39cb2
JordyBrown230 <Jordy.palacios.brown@est.una.ac.cr>
2023-11-05 23:36:24 -0600

    prueba de host

--- models/index.js
+++ models/index.js
@@ -1,11 +1,19 @@
 const Sequelize = require('sequelize');
+
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql',
-    define: {
-        freezeTableName: true
-      }
-  });  
+  host: process.env.DB_HOST,
+  dialect: 'mysql',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  },
+  define: {
+    freezeTableName: true
+  }
+});
+
 const db = {};
 
 db.Sequelize = Sequelize;
