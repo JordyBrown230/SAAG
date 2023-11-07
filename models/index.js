6895247b79afa46aba53c07505c09996151289cc
JordyBrown230 <Jordy.palacios.brown@est.una.ac.cr>
2023-11-07 02:41:59 -0600

    Cambio index.js
    En el .env: DB_NAME=SAAG
    DB_USER=SAAG
    DB_PASSWORD=Acib2023!!!
    DB_HOST=ing.mysql.database.azure.com
    DB_PORT=3306

--- models/index.js
+++ models/index.js
@@ -1,35 +1,20 @@
-const mysql = require('mysql');
-
-// Configuración de la conexión MySQL
-var dbConnection = mysql.createConnection({
-  host: 'ing.mysql.database.azure.com',
-  user: 'SAAG',
-  password: 'Acib2023!!!',
-  database: 'SAAG',
-  port: 3306,
-  ssl: { ca: fs.readFileSync('{ca-cert filename}') }
-});
-
-// Establecer la conexión a la base de datos
-dbConnection.connect(err => {
-  if (err) {
-    console.error('Error de conexión a la base de datos MySQL:', err);
-  } else {
-    console.log('Conexión a la base de datos MySQL exitosa');
-  }
-});
-
 const Sequelize = require('sequelize');
-const DB_NAME = 'SAAG';
+const {
+  DB_USER, DB_PASSWORD, DB_HOST, DB_NAME,DB_PORT
+} = process.env;
+console.log(DB_PORT)
 const sequelize = new Sequelize(DB_NAME, DB_USER, DB_PASSWORD, {
   host: DB_HOST,
+  port:DB_PORT,
   dialect: 'mysql',
   define: {
-    freezeTableName: true
+      freezeTableName: true
   },
-  // Pasar la conexión de MySQL como una opción adicional
   dialectOptions: {
-    socketPath: dbConnection
+      ssl: {
+          require: true,
+          rejectUnauthorized: false
+      }
   }
 });
 
@@ -38,15 +23,15 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.puesto = require('./puesto')(sequelize, Sequelize);
+db.puesto = require('./puesto')(sequelize,Sequelize);
 db.colaborador = require('./colaborador')(sequelize, Sequelize);
 db.solicitud = require('./solicitud')(sequelize, Sequelize);
 db.usuario = require('./usuario')(sequelize, Sequelize);
 db.ausencia = require('./ausencia')(sequelize, Sequelize);
-db.telefono = require('./telefonoEmpleado')(sequelize, Sequelize);
-db.horasExtra = require('./horasExtra')(sequelize, Sequelize);
-db.expediente = require('./expediente')(sequelize, Sequelize);
+db.telefono = require('./telefonoEmpleado')(sequelize,Sequelize);
+db.horasExtra = require('./horasExtra')(sequelize,Sequelize);
+db.expediente = require('./expediente')(sequelize,Sequelize);
 db.curriculum = require('./curriculum')(sequelize, Sequelize);
-db.titulo = require('./titulo')(sequelize, Sequelize);
+db.titulo = require('./titulo')(sequelize,Sequelize);
 
 module.exports = db;
