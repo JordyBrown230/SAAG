b6227f77fe95f940b2c5c52adba56876e7213a49
Darwin Javier Silva Gadea <115656897+Infil505@users.noreply.github.com>
2024-05-06 09:55:37 -0600

    cambio en cors

--- index.js
+++ index.js
@@ -9,6 +9,12 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(bodyParser.json());
+app.use((req, res, next)=>{
+  res.append('Access-Control-Allow-Origin', ['http://localhost:5173']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+})
 app.use(cors());
 
 // Define las rutas del API
