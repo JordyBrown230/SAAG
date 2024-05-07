bd70626734340b815995d419e56eb0f1513dd17d
Aaron101001 <71384668+Aaron101001@users.noreply.github.com>
2024-05-06 23:19:14 -0600

    Revert "cambios en cors"
    This reverts commit 4f9c4c46ae7b01cd2e886bef0032c7eb8ed948f5.

--- routes/usuario.routes.js
+++ routes/usuario.routes.js
@@ -1,31 +1,35 @@
 const express = require('express');
 const router = express.Router();
 const usuarioController = require('../controllers/usuario.controller');
-const { authenticateToken, authorizeRoles } = require('../middlewares/auth.middleware');
-const { auditLogin, auditLogout } = require('../middlewares/audit.middleware');
+const { authenticateToken, authorizeRoles} = require('../middlewares/auth.middleware');
+const {auditLogin, auditLogout} = require('../middlewares/audit.middleware');
 const cors = require('cors');
 
-const corsOptions = {
-  origin: ['http://localhost:5173/'],
-  methods: ['PUT'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-};
+router.options('*', cors());
 
-router.options('*', cors(corsOptions));
-
-router.use(cors(corsOptions));
+router.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    res.sendStatus(200);
+  } else {
+    next();
+  }
+});
 
 router.get('/admin', authenticateToken, authorizeRoles(['admin']), (req, res) => {
   res.json({ message: 'Acceso permitido para el rol de administrador' });
 });
 
+// No necesita verificación
 router.post('/login/', usuarioController.login, auditLogin);
 
-router.post('/logout/:token', usuarioController.logout, auditLogout);
+router.post('/logout/:token',  usuarioController.logout, auditLogout);
 
-router.post('/refresh/:token', usuarioController.refreshToken);
+router.post('/refresh/:token',  usuarioController.refreshToken);
 
-router.post('/agregar-usuario/', authenticateToken, authorizeRoles(['admin']), usuarioController.create);
+router.post('/agregar-usuario/',  authenticateToken, authorizeRoles(['admin']), usuarioController.create);
 
 router.get('/usuarios/', authenticateToken, authorizeRoles(['admin']), usuarioController.findAll);
 
