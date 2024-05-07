50673bd4458ee24ab2788f38b88d324d3fa231ce
Aaron101001 <71384668+Aaron101001@users.noreply.github.com>
2024-05-06 23:19:44 -0600

    Revert "mejoras"
    This reverts commit 3959353d473c0437d0b42486f62750ff39e655ae.

--- controllers/gmail.controller.js
+++ controllers/gmail.controller.js
@@ -2,10 +2,10 @@ const transporter = require('../models/gmail');
 // funcion global para enviar correos desde cualquier controller
 
 const enviarCorreo = async (toList, subject, htmlContent, fromt) => {
-    console.log("esta enviando correos");
     const from = fromt;  // este es el correo que envia ya que no tenemos uno de la empresa por ende es uno mio
     // Verifica si toList es una cadena (un solo correo) o una matriz (varios correos)
     const destinatarios = Array.isArray(toList) ? toList.join(', ') : toList;
+    console.log(destinatarios);
     try {
         await transporter.sendMail({    //estructura de los correos 
             from: from,
